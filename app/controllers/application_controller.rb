4cf7c063f3fc18e47d2807defe0738f290caa1b5
Phillip Kessels <info@phikes.de>
2014-10-31 12:08:17 +0100

    Now if a record is not found a 404 is issued

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+  protected
+  def not_found
+    render status: 404, json: ''
+  end
 end
---
+++ spec/controllers/application_controller.rb
@@ -0,0 +1,3 @@
+describe ApplicationController do
+  it 'renders a 404, when a record is not found'
+end
