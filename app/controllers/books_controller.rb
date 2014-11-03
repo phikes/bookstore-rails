d8a275d987593ffeb5cf2638de770e5660780032
Marko Kajzer <marko.kajzer@rwth-aachen.de>
2014-11-03 10:39:11 +0100

    Added initialization of http headers to books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,6 @@
 class BooksController < ApplicationController
+  before_filter :set_headers
+
   def index
     @books = Book.all
     render json: @books.to_json
@@ -20,5 +22,18 @@ class BooksController < ApplicationController
   private
   def book_params
     params.require(:book).permit :title, :author, :isbn, :rating
+  end
+
+  def set_headers
+    if request.headers["HTTP_ORIGIN"]
+    # better way check origin
+    # if request.headers["HTTP_ORIGIN"] && /^https?:\/\/(.*)\.some\.site\.com$/i.match(request.headers["HTTP_ORIGIN"])
+      headers['Access-Control-Allow-Origin'] = request.headers["HTTP_ORIGIN"]
+      headers['Access-Control-Expose-Headers'] = 'ETag'
+      headers['Access-Control-Allow-Methods'] = 'GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD'
+      headers['Access-Control-Allow-Headers'] = '*,x-requested-with,Content-Type,If-Modified-Since,If-None-Match,Auth-User-Token'
+      headers['Access-Control-Max-Age'] = '86400'
+      headers['Access-Control-Allow-Credentials'] = 'true'
+    end
   end
 end
